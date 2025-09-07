0a31185108d25c2ab5e8fd4b02e29c75a01cddd1
Владислав <166236897+VladBordyug@users.noreply.github.com>
2025-09-07 14:43:41 +0300

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,25 +1,24 @@
-
+// Масив для зберігання товарів у кошику
 let cart = [];
 
-
+// Функція для додавання товару в кошик
 function addToCart(productName) {
     cart.push(productName);
-    console.log(cart);  
+    console.log(cart);  // Логування кошика для перевірки
 
-    
+    // Оновлення відображення кількості товарів у кошику
     updateCartDisplay();
 }
 
-
+// Оновлення відображення кількості товарів у кошику
 function updateCartDisplay() {
     const cartCount = document.querySelector('.cart-items');
     cartCount.textContent = `Items in Cart: ${cart.length}`;
 }
 
-
+// Додаємо обробник подій на кнопку "ADD TO CART"
 const addToCartBtn = document.querySelector('.add-to-cart-btn');
 addToCartBtn.addEventListener('click', function () {
     const productName = "T-shirt";
     addToCart(productName);
 });
-
